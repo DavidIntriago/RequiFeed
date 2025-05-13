29b62cd9610b686424c27a97c59302946aa3870a
DavidIntriago <david.intriago@unl.edu.ec>
2025-05-13 15:10:47 -0500

    Refactor AppModule and main.ts

--- requi-feed_backend/src/app.module.ts
+++ requi-feed_backend/src/app.module.ts
@@ -1,15 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaService } from './db/prisma.service';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UsuarioModule } from './modules/usuario/usuario.module';
+import { CuentaModule } from './modules/cuenta/cuenta.module';
+import { RolService } from './modules/rol/rol.service';
+import { UsuarioService } from './modules/usuario/usuario.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+      isGlobal: true,
+    }),
+    UsuarioModule,
+    CuentaModule,
     
   ],
-  controllers: [AppController],
   providers: [{provide: PrismaService,
     useFactory: () => PrismaService.getInstance()
-  }]
+  }, RolService, UsuarioService],
 })
-export class AppModule {}
+
+export class AppModule {
+  constructor(
+    private readonly rolService: RolService,
+    private readonly userService: UsuarioService,
+  ) {
+    this.rolService.revisionRol();
+    this.userService.createAdmin();
+  }
+}
