8d3d86fbeca21f5604f62fff765b16bf60c61116
DavidIntriago <david.intriago@unl.edu.ec>
2025-05-19 20:29:42 -0500

    connect registry with frontend

--- requi-feed_frontend/src/app/authentication/signup/page.tsx
+++ requi-feed_frontend/src/app/authentication/signup/page.tsx
@@ -18,15 +18,82 @@ import { useColorScheme, useMediaQuery } from '@mantine/hooks';
 import { Metadata } from 'next';
 import { Surface } from '@/components';
 import classes from './page.module.css';
+import { Select } from '@mantine/core';
+import { useState } from 'react';
+import { post_api } from '@/hooks/Conexion';
+import { useRouter } from 'next/navigation';
 
 function Page() {
-  const theme = useMantineTheme();
-  const colorScheme = useColorScheme();
-  const mobile_match = useMediaQuery('(max-width: 425px)');
-
+ 
   const LINK_PROPS: TextProps = {
     className: classes.link,
   };
+  const { push } = useRouter();
+  const [email, setEmail] = useState("");
+  const [contrasenia, setContrasenia] = useState("");
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [ocupacion, setOcupacion] = useState("");
+  const [area, setArea] = useState("");
+  const [foto, setFoto] = useState(null);
+  const [confirmContrasenia, setConfirmContrasenia] = useState("");
+  
+const [errors, setErrors] = useState<Record<string, string>>({});
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  const validateForm = () => {
+     const newErrors: Record<string, string> = {};
+
+    if (!nombre.trim()) newErrors.nombre = "El nombre es obligatorio.";
+    if (!apellido.trim()) newErrors.apellido = "El apellido es obligatorio.";
+    if (!email.trim() || !emailRegex.test(email))
+      newErrors.email = "Correo electrónico no válido.";
+    if (!contrasenia.trim() || contrasenia.length < 6)
+      newErrors.contrasenia = "La contraseña debe tener al menos 6 caracteres.";
+    if (!ocupacion.trim()) newErrors.ocupacion = "La ocupación es obligatoria.";
+    if (!area.trim()) newErrors.area = "El área es obligatoria.";
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleRegister = async (e) => {
+    console.log("handleRegister");
+    e.preventDefault();
+    if (!validateForm())return;
+    
+    if (contrasenia !== confirmContrasenia) {
+      setErrors({ ...errors, contrasenia: "Las contraseñas no coinciden." });
+      return;
+    }
+    console.log("handleRegister2");
+    const body = {
+      nombre,
+      apellido,
+      email,
+      contrasenia,
+      ocupacion,
+      area,
+    };
+    try {
+      const response = await post_api('cuenta/registry', body);
+      if (response?.error) {
+        setErrors({ ...errors, email: response.error });
+        return;
+      }
+      push(PATH_AUTH.signin);
+      
+
+    } catch (error) {
+      console.error("Error al registrar el usuario:", error);
+      setErrors({ ...errors, email: "Error de conexión o servidor. Intenta más tarde." });
+      
+    }
+  }
+    
+
+
 
   return (
     <>
@@ -47,20 +114,64 @@ function Page() {
             placeholder="John"
             required
             classNames={{ label: classes.label }}
+            onChange={(e) => setNombre(e.target.value)}
+            error={errors.nombre}
+            
           />
           <TextInput
             label="Last name"
             placeholder="Doe"
             required
             classNames={{ label: classes.label }}
+            onChange={(e) => setApellido(e.target.value)}
+            error={errors.apellido}
           />
         </Flex>
+        <Select
+          w="100%"
+          mt="md"
+
+          label="Ocupación"
+          placeholder="Selecciona una ocupación"
+          required
+          data={[
+            { value: 'estudiante', label: 'Estudiante' },
+            { value: 'docente', label: 'Docente' },
+            { value: 'otro', label: 'Otro' },
+          ]}
+          value={ocupacion}
+          classNames={{ label: classes.label }}
+          onChange={(value) => setOcupacion(value)}
+          error={errors.ocupacion}
+        />
+        <Select
+          w="100%"
+          mt="md"
+          label="Area de trabajo"
+          placeholder="Seleccione una area de trabajo"
+          required
+          data={[
+            { value: 'computacion', label: 'Computación' },
+            { value: 'administrativa', label: 'Administrativa' },
+            { value: 'investigativa', label: 'Investigativa' },
+            { value: 'otro', label: 'Otro' },
+
+          ]}
+          value={area}
+          classNames={{ label: classes.label }}
+          onChange={(value) => setArea(value)}
+          error={errors.area}
+/>
+
+
         <TextInput
           label="Email"
           placeholder="you@mantine.dev"
           required
           mt="md"
           classNames={{ label: classes.label }}
+          onChange={(e) => setEmail(e.target.value)}
+          error={errors.email}
         />
         <PasswordInput
           label="Password"
@@ -68,6 +179,8 @@ function Page() {
           required
           mt="md"
           classNames={{ label: classes.label }}
+          onChange={(e) => setContrasenia(e.target.value)}
+          error={errors.contrasenia}
         />
         <PasswordInput
           label="Confirm Password"
@@ -75,13 +188,13 @@ function Page() {
           required
           mt="md"
           classNames={{ label: classes.label }}
+          onChange={(e) => setConfirmContrasenia(e.target.value)}
+          error={contrasenia !== confirmContrasenia ? "Las contraseñas no coinciden." : ""}
         />
         <Button
+          onClick={handleRegister}
           fullWidth
-          mt="xl"
-          component={Link}
-          href={PATH_DASHBOARD.default}
-        >
+          mt="xl"        >
           Create account
         </Button>
         <Center mt="md">
