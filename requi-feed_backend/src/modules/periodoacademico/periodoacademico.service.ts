bf7e0aee0cb85bdab1638b36d7d30b3aa0d8c39d
DavidIntriago <david.intriago@unl.edu.ec>
2025-05-26 05:57:55 -0500

    Implement  operations for academic periodsand validations

--- requi-feed_backend/src/modules/periodoacademico/periodoacademico.service.ts
+++ requi-feed_backend/src/modules/periodoacademico/periodoacademico.service.ts
@@ -8,24 +8,85 @@ export class PeriodoacademicoService {
   constructor(private prisma: PrismaService) { }
 
   async createPeriodoAcademico(periodoAcademicoDto: periodoAcademicoDto) {
+    const periodosExistente = await this.prisma.periodoAcademico.findFirst({
+      where: {
+        fechaInicio: periodoAcademicoDto.fechaInicio,
+        fechaFin: periodoAcademicoDto.fechaFin,
+      },
+    });
+    if (periodosExistente) {
+      return {
+        message: 'Ya existe un periodo academico con las mismas fechas',
+      };
+    }else{ 
+    
     const periodoAcademico = await this.prisma.periodoAcademico.create({
       data: {
         nombre: periodoAcademicoDto.nombre,
-        PeriodoAcademico: periodoAcademicoDto.PeriodoAcademico,
         modalidad: periodoAcademicoDto.modalidad,
+        fechaInicio: periodoAcademicoDto.fechaInicio,
+        fechaFin: periodoAcademicoDto.fechaFin,
       },
     });
+
     return {
+    
       data: periodoAcademico,
+    }
+  }}
+
+  async findUltimoPeriodoAcademico() {
+    const periodactual = await this.prisma.periodoAcademico.findFirst({
+      orderBy: {
+        fechaInicio: 'desc',
+      },
+    });
+    return{
+      data: periodactual
     }
   }
 
-  findUltimoPeriodoAcademico() {
-    return this.prisma.periodoAcademico.findFirst({
+  async findAll() {
+    const periodos = await this.prisma.periodoAcademico.findMany({
       orderBy: {
-        id: 'desc',
+        fechaInicio: 'desc',
       },
     });
+    return {
+      data: periodos,
+    }
   }
+
+  async updatePeriodoAcademico(id: number, periodoAcademicoDto: periodoAcademicoDto) {
+  const periodoExistente = await this.prisma.periodoAcademico.findFirst({
+    where: {
+      fechaInicio: periodoAcademicoDto.fechaInicio,
+      fechaFin: periodoAcademicoDto.fechaFin,
+      NOT: {
+        id,  },
+    },
+  });
+
+  if (periodoExistente) {
+    return {
+      message: 'Ya existe un periodo académico con las mismas fechas',
+    };
+  }
+
+  const periodoActualizado = await this.prisma.periodoAcademico.update({
+    where: { id },
+    data: {
+      nombre: periodoAcademicoDto.nombre,
+      modalidad: periodoAcademicoDto.modalidad,
+      fechaInicio: periodoAcademicoDto.fechaInicio,
+      fechaFin: periodoAcademicoDto.fechaFin,
+    },
+  });
+
+  return {
+    data: periodoActualizado,
+  };
+}
+
 
 }
