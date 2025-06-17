9789a164c7ce0659abd3c06ea783049ca16c5e52
JhairAgila <jhairagil@gmail.com>
2025-06-17 09:36:15 -0500

    fix/projects view

--- requi-feed_frontend/src/app/estudiante/grupo/proyectos/page.tsx
+++ requi-feed_frontend/src/app/estudiante/grupo/proyectos/page.tsx
@@ -68,11 +68,17 @@ function Projects() {
   } = useFetchData('/mocks/Projects2.json');
 
   const [projects, setProjects] = useState<Project[] | null>(null);
-  
+  const [idGrupo, setIdGrupo] = useState<string>('');
+  const rol = get('rol');
+  const external_id = get('external_id');
+
   const getProjects = async () => {
     try {
-      const {data} = await get_api(`proyecto`);
-      setProjects(data);
+      const dataCuenta :any = await get_api(`grupo/user/${external_id}`);
+      console.log(dataCuenta.data.grupo.external_id)
+      const grupo : any = await get_api(`proyecto/grupo/${dataCuenta.data.grupo.external_id}`); 
+      setIdGrupo(dataCuenta.data.grupo.external_id);
+      setProjects(grupo.data);
     } catch (error:any) {
       mensajes("Error", error.response?.data?.customMessage || "No se ha podido obtener el usuario", "error");
     }
@@ -102,20 +108,20 @@ function Projects() {
       <Container fluid>
         <Stack gap="lg">
           <PageHeader title="Proyectos" breadcrumbItems={items} />
-          {/* {rol == 'LIDER' ? (
+          {rol == 'LIDER' ? (
             <Button
             mx="xs"
             radius="sm"
             variant="gradient"
             leftSection={<IconPlus size="18" />}
             onClick={() => {
-              router.push(`/estudiante/grupo/proyectos/create/${id}`);
+              router.push(`/estudiante/grupo/proyectos/create/${idGrupo}`);
               // createTask(column.id);
             }}
           >
             Crear Proyecto
           </Button>
-          ) : <></> } */}
+          ) : <></> }
           
           {projectsError ? (
             <ErrorAlert
