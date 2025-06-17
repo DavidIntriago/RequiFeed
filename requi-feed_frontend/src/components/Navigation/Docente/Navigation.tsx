249b84e0a4878d719981906e09df5a3663fe70a9
DavidIntriago <david.intriago@unl.edu.ec>
2025-06-17 12:09:53 -0500

    Add Calificaciones link to Navigation

--- requi-feed_frontend/src/components/Navigation/Docente/Navigation.tsx
+++ requi-feed_frontend/src/components/Navigation/Docente/Navigation.tsx
@@ -41,6 +41,7 @@ const mockdata = [
       { label: 'Proyectos', icon: IconBriefcase, link: PATH_DOCENTE.proyectos },
       { label: 'Usuarios', icon: IconListDetails, link: PATH_DOCENTE.usuarios },
       {label: 'Grupos', icon: IconLayersSubtract, link: PATH_DOCENTE.grupos},
+      { label: 'Calificaciones', icon: IconFileInvoice, link: PATH_DOCENTE.calificaciones },
 
   ],
   },
